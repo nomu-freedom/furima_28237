612327d7bb9b553edc69525128c0d76cc644bfce
nomu-freedom <68408966+nomu-freedom@users.noreply.github.com>
2020-08-05 21:42:38 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+  with_options presence: true do
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/} do
+      validates :family_name
+      validates :last_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/} do
+      validates :family_name_reading
+      validates :last_name_reading
+    end
+    with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i} do
+      validates :password
+      validates :password_confirmation
+    end
+    validates :email, uniqueness: { case_sensitive: false },
+                      format: { with: /\A[a-zA-Z0-9_\#!$%&`'*+\-{|}~^\/=?\.]+@[a-zA-Z0-9][a-zA-Z0-9\.-]+\z/}
+    validates :nickname, :birthday
+  end
 end
