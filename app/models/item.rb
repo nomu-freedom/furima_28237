c3ca42c041e723348ff7a4e122c21796bb16966a
nomu-freedom <68408966+nomu-freedom@users.noreply.github.com>
2020-08-07 18:49:45 +0900

    JavaScriptファイルの準備

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,9 +6,9 @@ class Item < ApplicationRecord
   validates :category, :delivery_fee, :shipping_origin, :status, :until_shipping
   end
 
-    with_options numericality: { other_than: 1 } do
-    validates :category_id, :delivery_fee_id, :shipping_origin_id, :status_id, :until_shipping_id
-    end
+  with_options numericality: { other_than: 1 } do
+  validates :category_id, :delivery_fee_id, :shipping_origin_id, :status_id, :until_shipping_id
+  end
 
   belongs_to :user
 end
