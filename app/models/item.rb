4a2275acf3078fa42aed8f44b0fba994c72237d6
nomu-freedom <68408966+nomu-freedom@users.noreply.github.com>
2020-08-07 12:31:11 +0900

    ActiveHashデータ内容記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :delivery, :shipping_origin, :status, :until_shipping
+
+  with_options precense: true do
+  validates :category, :delivery, :shipping_origin, :status, :until_shipping
+  end
+
+    with_options numericality: { other_than: 1 } do
+    validates :category_id, :delivery_id, :shipping_origin_id, :status_id, :until_shipping_id
+    end
+
   belongs_to :user
 end
