b755c179490ed56511fbda7f485d7de2beef3e48
nomu-freedom <68408966+nomu-freedom@users.noreply.github.com>
2020-08-10 15:42:33 +0900

    ログインしていないユーザーのリダイレクト実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,30 @@
 class ItemsController < ApplicationController
+before_action :move_to_index, expect: [:index, :show]
+
+  def index
+    @item = Item.all
+  end
+
   def new
     @item = Item.new
   end
   
   def create
-    
+    if Item.create(item_params)
+      redirect_to root_path
+    end
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :explanation, :image, :category_id, :status_id, :delivery_fee_id, :until_shipping_id, :shipping_origin_id, :price )
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
