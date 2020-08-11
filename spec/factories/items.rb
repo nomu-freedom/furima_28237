05ea37092a19d61609379e780ef1f6aaee2b3517
nomu-freedom <68408966+nomu-freedom@users.noreply.github.com>
2020-08-11 11:31:56 +0900

    単体テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    image {Faker::Lorem.sentence}
+    name {Faker::Game.title }
+    explanation {Faker::Lorem.sentence}
+    category_id {Faker::Number.within(range: 2..11)}
+    status_id {Faker::Number.within(range: 2..3)}
+    delivery_fee_id {Faker::Number.within(range: 2..48)}
+    shipping_origin_id {Faker::Number.within(range: 2..7)}
+    until_shipping_id {Faker::Number.within(range: 2..4)}
+    price {Faker::Number.within(range: 300..9999999)}
+    association :user
   end
 end
