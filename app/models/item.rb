813fd8bd49737dff956b6d3238fea7655444351d
nomu-freedom <68408966+nomu-freedom@users.noreply.github.com>
2020-08-11 11:30:35 +0900

    バリデーションの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,32 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :delivery_fee, :shipping_origin, :status, :until_shipping
-
-  with_options precense: true do
-  validates :category, :delivery_fee, :shipping_origin, :status, :until_shipping
+  
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :explanation
+    #validates :category
+    #validates :delivery_fee
+    #validates :shipping_origin
+    #validates :status
+    #validates :until_shipping
+    validates :price, numericality: { only_integer: true, :greater_than_or_equal_to => 300, :less_than => 10000000 }
   end
 
+  
   with_options numericality: { other_than: 1 } do
-  validates :category_id, :delivery_fee_id, :shipping_origin_id, :status_id, :until_shipping_id
+  validates :category_id
+  validates :delivery_fee_id
+  validates :shipping_origin_id
+  validates :status_id
+  validates :until_shipping_id
   end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :shipping_origin
+  belongs_to_active_hash :status
+  belongs_to_active_hash :until_shipping
 
   belongs_to :user
   has_one_attached :avatar
