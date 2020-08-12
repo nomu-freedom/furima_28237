58aaa57eadf3b7684195539c1f5e91c9c34cb73c
nomu-freedom <68408966+nomu-freedom@users.noreply.github.com>
2020-08-12 11:02:59 +0900

    itemsテーブルのimageカラムの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_110215) do
+ActiveRecord::Schema.define(version: 2020_08_11_132928) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 2020_08_07_110215) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "image", null: false
     t.string "name", null: false
     t.text "explanation", null: false
     t.integer "category_id", null: false
