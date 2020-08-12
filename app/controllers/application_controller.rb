aad9e11bb5ebf46f5080597a979675a3b2b4e7aa
nomu-freedom <68408966+nomu-freedom@users.noreply.github.com>
2020-08-12 11:22:44 +0900

    BASIC認証のコメントアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
-  before_action :authenticate_user!
+  #before_action :basic_auth
+  #before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
@@ -9,9 +9,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :last_name, :family_name_leading, :last_name_leading, :birthday])
   end
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"]  && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
+  #def basic_auth
+    #authenticate_or_request_with_http_basic do |username, password|
+      #username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    #end
+  #end
 end
