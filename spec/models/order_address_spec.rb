d3fa28801e8025c0028cdcc6325030e5845ff85f
nomu-freedom <68408966+nomu-freedom@users.noreply.github.com>
2020-08-17 16:34:46 +0900

    単体テストユーザー2人の定義

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -3,7 +3,12 @@ require 'rails_helper'
 RSpec.describe OrderAddress, type: :model do
   describe "購入情報の保存" do
     before do
-      @order_address = FactoryBot.build(:order_address)
+      @buyer = FactoryBot.build(:user)
+      @seller = FactoryBot.build(:user)
+      item = FactoryBot.build(:item, user_id: @seller.id)
+      item.image = fixture_file_upload("/files/afdgbf.jpg")
+      item.save
+      @order_address = FactoryBot.build(:order_address, user_id: @buyer.id, item_id: item.id)
     end
 
     context "購入がうまくいくとき" do
@@ -13,8 +18,8 @@ RSpec.describe OrderAddress, type: :model do
     end
 
     context "購入がうまくいかないとき" do
-      it "郵便番号の情報が正しくないとき" do
-        @order_address.postal_code = 1234-432
+      it "郵便番号は半角のハイフンを含んだ正しい形式でないと保存できないこと" do
+        @order_address.postal_code = 1234432
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Postal code is invalid")
       end
@@ -33,6 +38,11 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("City can't be blank")
       end
+      it "市区町村の情報が平仮名、カタカナ、漢字以外の時" do
+        @order_address.city = "tokyo"
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("City is invalid")
+      end
       it "番地の情報が空の時" do
         @order_address.house_number = ""
         @order_address.valid?
@@ -40,6 +50,16 @@ RSpec.describe OrderAddress, type: :model do
       end
       it "電話番号の情報が正しくない時" do
         @order_address.phone_number = 123456789009
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+      end
+      it "電話番号の情報にハイフンが用いられているとき" do
+        @order_address.phone_number = 123-4568-9009
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+      end
+      it "電話番号の情報が11桁以上の時" do
+        @order_address.phone_number = 1232145689009
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Phone number is invalid")
       end
